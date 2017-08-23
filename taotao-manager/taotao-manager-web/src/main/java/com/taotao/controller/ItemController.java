192ffb2392de3d5084dd1a223074f6b18c4eab2c
niemengquan <niemengquan@126.com>
2017-08-23 17:33:20 +0800

    商品添加功能：同时添加商品的描述信息

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,10 +1,14 @@
 package com.taotao.controller;
 
 import com.taotao.common.pojo.EasyUiListResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -34,5 +38,19 @@ public class ItemController {
         }
         return new EasyUiListResult();
     }
+
+    /**
+     * 保存一条商品信息
+     * @param item
+     * @param itemDesc
+     * @return
+     */
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    @ResponseBody
+    public TaotaoResult addItem(TbItem item, TbItemDesc itemDesc){
+        TaotaoResult result=this.itemService.addItem(item,itemDesc);
+        return result;
+    }
+
 
 }
