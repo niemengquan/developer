d119bd1cdd57c9b90d9054bbea05f7be3fcb872b
niemengquan <niemengquan@126.com>
2017-08-29 17:38:54 +0800

    item  pojo add

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/pojo/Item.java
@@ -0,0 +1,82 @@
+package com.taotao.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Item {
+
+	private String id;
+	private String title;
+	private String sellPoint;
+	private long price;
+	private String image;
+	private String categoryName;
+	private String itemDes;
+
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSellPoint() {
+		return sellPoint;
+	}
+
+	public void setSellPoint(String sellPoint) {
+		this.sellPoint = sellPoint;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public void setPrice(long price) {
+		this.price = price;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getItemDes() {
+		return itemDes;
+	}
+
+	public void setItemDes(String itemDes) {
+		this.itemDes = itemDes;
+	}
+
+	@JsonIgnore
+	public String[] getImages() {
+		if (image != null) {
+			String[] images = image.split(",");
+			return images;
+		}
+		return null;
+	}
+	
+	
+}
