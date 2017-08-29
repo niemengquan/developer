e52ca0580afaec5615d8b61ae2b2d2822a8f4656
niemengquan <niemengquan@126.com>
2017-08-29 14:05:08 +0800

    移除为使用的导入包

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ContentController.java
@@ -2,17 +2,13 @@ package com.taotao.controller;
 
 import com.taotao.common.pojo.EasyUiListResult;
 import com.taotao.common.pojo.TaotaoResult;
-import com.taotao.common.utils.ExceptionUtil;
 import com.taotao.pojo.TbContent;
 import com.taotao.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
  * Created by niemengquan on 2017/8/25.
