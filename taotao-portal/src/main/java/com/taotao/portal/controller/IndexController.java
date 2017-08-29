09a360ae0f9edae3ea00ae69edc9524a3d2ea74c
niemengquan <niemengquan@126.com>
2017-08-29 14:35:07 +0800

    增加大广告位的内容的获取

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.taotao.portal.controller;
 
+import com.taotao.portal.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private ContentService contentService;
     @RequestMapping("/index")
     private String showIndex(Model model){
+        String contentListForBigAdShow = this.contentService.getContentListForBigAdShow();
+        model.addAttribute("ad1",contentListForBigAdShow);
         return "index";
     }
 }
