b3d6dd490b4cf3b1dcb2b4f7a280d0833e478067
niemengquan <niemengquan@126.com>
2019-10-17 14:05:58 +0800

    test分支功能

---
+++ javabasicstudy/src/main/java/com/nmq/javaagent/Test.java
@@ -0,0 +1,13 @@
+package com.nmq.javaagent;
+
+/**
+ * @author niemengquan
+ * @create 2019/8/19
+ * @modifyUser
+ * @modifyDate
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
